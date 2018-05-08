57ba9276834db66eef46404bc58a1449eb1d626f
Manoj <mmasane@incipientcorp.com>
2018-05-08 16:21:16 +0530

    Intial commit

---
+++ app/src/main/java/com/app/moviesapp/activities/HomeActivity.kt
@@ -0,0 +1,13 @@
+package com.app.moviesapp.activities
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.app.moviesapp.R
+
+class HomeActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_home)
+    }
+}
